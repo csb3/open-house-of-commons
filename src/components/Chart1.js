eed356fd1a83b981aaf9abf9e16861260cef31e5
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-14 17:51:13 -0600

    add sample chart

---
+++ src/components/Chart1.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Chartjs from 'chart.js';
+
+const chartConfig = {
+  type: 'pie',
+  data: {
+      labels: ['Yes', 'No'],
+      datasets: [{
+          label: '# of Votes',
+          data: [12, 19],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+    legend: {
+      display: false
+  },
+  }
+}
+
+export default function Chart1() {
+  const chartContainer = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
+
+  useEffect(() => {
+    if (chartContainer && chartContainer.current) {
+      const newChartInstance = new Chartjs(chartContainer.current, chartConfig);
+      setChartInstance(newChartInstance);
+    }
+  }, [chartContainer]);
+
+  return (
+    <div>
+      <canvas ref={chartContainer} />
+    </div>
+  );
+};
--- src/stories/chart1.stories.js
+++ src/stories/chart1.stories.js
@@ -2,20 +2,22 @@ import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import Nav from "../components/Nav";
+import Chart1 from "../components/Chart1";
 ;
 
 //👇 This default export determines where your story goes in the story list
 export default {
-  title: 'Nav',
-  component: Nav,
+  title: 'Chart1',
+  component: Chart1,
 };
 
 //👇 We create a “template” of how args map to rendering
-const Template = (args) => <Nav {...args} />;
+const Template = (args) => <Chart1 {...args} />;
 
 export const FirstStory = Template.bind({});
 
 FirstStory.args = {
   /*👇 The args you need here will depend on your component */
 };
+
+
