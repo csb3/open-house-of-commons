b74751b8c1df803215d7af5c83fa2d313d81c0c9
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-19 21:13:38 -0600

    add party name and adjusted styling for mp-vote grid view

--- src/components/MPVote.js
+++ src/components/MPVote.js
@@ -10,7 +10,10 @@ import MpListItem from './MpListItem';
 export default function MPVote(props) {
   const mpSummary = props.voteInfo.map(vote => {
     const newClass = classnames('mp-vote', {"yea": vote.voted_yea, "nay": vote.voted_nay, "paired": vote.vote_paired, "did-not-vote": !(vote.voted_nay || vote.voted_yea || vote.vote_paired)});
-    return (<div className={newClass}>{`${vote.first_name} ${vote.last_name}`}</div>);
+    return (<div className={newClass}>
+      <p>{`${vote.first_name} ${vote.last_name}`}</p>
+      <p>{`${vote.party_name}`}</p>
+      </div>);
   });
   return (
     <div>
