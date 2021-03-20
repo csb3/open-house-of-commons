3d7ec86595a340a4dc07332ff92868985919d88d
Chanoon Na <chanoonna@gmail.com>
2021-03-20 15:22:45 -0700

    temporary commit for quick fix from main

--- src/components/mps/Mp.js
+++ src/components/mps/Mp.js
@@ -9,7 +9,15 @@ import axios from 'axios';
 export default function Mp() {
   const { id } = useParams();
   const [mp, setMp] = useState({
-    profile: {},
+    profile: {
+      id: 0,
+      first_name: '',
+      last_name: '',
+      party_name: '',
+      constituency: '',
+      location: '',
+      thumbnail: '',
+    },
     voted: [],
     sponsored: [],
   });
@@ -17,16 +25,23 @@ export default function Mp() {
   useEffect(() => {
     Promise.resolve(
       axios.get(`/api/mps/${id}`)
-        .then(result => {
-          setMp(() => ({...result.data}))
+        .then(res => {
+          const url = getPhotoURL(res.data.profile);
+          setMp(() => ({...res.data, profile: { ...res.data.profile, thumbnail: url}}))
         })
     );
   }, []);
 
   return (
     <section className="mp-page">
+      <header>
+        <h1>{`${mp.profile.first_name} ${mp.profile.last_name}`}</h1>
+      </header>
       <aside>
-        
+        <img src={mp.profile.thumbnail} alt={`${mp.profile.first_name} ${mp.profile.last_name}`} />
+      </aside>
+      <aside>
+
       </aside>
     </section>
   );
