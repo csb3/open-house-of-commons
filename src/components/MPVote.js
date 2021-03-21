e35b45e640cfca898eeb39e4e583d32316702ac1
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-20 18:29:27 -0600

    edit mp grid formatting

--- src/components/MPVote.js
+++ src/components/MPVote.js
@@ -3,20 +3,15 @@ import classnames from "classnames";
 
 export default function MPVote(props) {
   const mpSummary = props.voteInfo.map(vote => {
-    const newClass = classnames('mp-vote', {"yea": vote.voted_yea, "nay": vote.voted_nay, "paired": vote.vote_paired, "did-not-vote": !(vote.voted_nay || vote.voted_yea || vote.vote_paired)});
+    const newClass = classnames('mp-vote', vote.party_name.replace(/\s/g,''), {"yea": vote.voted_yea, "nay": vote.voted_nay, "paired": vote.vote_paired, "did-not-vote": !(vote.voted_nay || vote.voted_yea || vote.vote_paired)});
     return (<div className={newClass}>
-      <p>{`${vote.first_name} ${vote.last_name}`}</p>
-      <p>{`${vote.name}`}</p>
-      <p>{`${vote.party_name}`}</p>
       </div>);
   });
   return (
-    <div>
-      <h1>MP Vote Overview</h1>
     <section className="mp-vote-container">
+      <h1>MP Vote Overview</h1>
       <br />
       {mpSummary}
     </section>
-    </div>
   )
 }
