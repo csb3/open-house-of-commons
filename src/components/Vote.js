f53eed643e011ccd78faad529eb8cfe5cb11fd64
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-20 20:17:16 -0600

    remove unused chart files

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -14,7 +14,7 @@ import './../components/Vote.scss';
 
 export default function Vote(props) {
   const {id} = useParams(); // req.params.shortURL
-  const [vote, setVote] = useState();
+  const [vote, setVote] = useState({});
   
   useEffect(() => {
     axios.get(`/api/votes/${id}`)
