92b311f3804d32002c55bcdb68c812ec2c53a01d
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-23 14:50:59 -0600

    fix icon mixup in sponlistitem

--- src/components/mps/SponListItem.js
+++ src/components/mps/SponListItem.js
@@ -17,8 +17,8 @@ export default function SponListItem(props) {
           <div class="vote-list-header">
             <div class="vote-list-title">{`Vote No. ${props.vote_num}`}</div>
           
-            {props.result==="Negatived" && <div class="vote-disagree">Result: Negatived <FontAwesomeIcon icon={faCheck} /></div>}
-            {props.result==="Agreed To" && <div class="vote-agree">Result: Agreed To <FontAwesomeIcon icon={faTimes} /></div>}
+            {props.result==="Negatived" && <div class="vote-disagree">Result: Negatived <FontAwesomeIcon icon={faTimes} /></div>}
+            {props.result==="Agreed To" && <div class="vote-agree">Result: Agreed To <FontAwesomeIcon icon={faCheck} /></div>}
           </div>
         
           <div class="vote-list-summary">{props.summary}</div>
