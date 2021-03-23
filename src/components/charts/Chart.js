283595e8045fe4e658131202d0acfa026217cc8e
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-23 14:26:02 -0600

    make view of single chart bigger

--- src/components/charts/Chart.js
+++ src/components/charts/Chart.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Chartjs from 'chart.js';
+import classnames from "classnames";
 import './ChartGrid.scss';
 const getChartParams = require('../helpers/chart-helper');
 
@@ -8,7 +9,7 @@ export default function Chart(props) {
   const chartConfig = getChartParams({...props});
   const [chartInstance, setChartInstance] = useState(null);
   const chartContainer = useRef(null);
-  
+  const chartClassName = classnames("chart-grid--item", { "display": props.display})
   useEffect(() => {
     if (chartContainer && chartContainer.current) {
       const newChartInstance = new Chartjs(chartContainer.current, chartConfig);
@@ -17,7 +18,7 @@ export default function Chart(props) {
   }, [chartContainer, props]);
 
   return (
-    <div className="chart-grid--item">
+    <div className={chartClassName}>
       <canvas ref={chartContainer} />
     </div>
   );
