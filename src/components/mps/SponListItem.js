bd4e730ba6b8cd89823a8004c102cb2ec7cc94d1
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-23 14:27:27 -0600

    update 'disagreed to' to 'negatived'

--- src/components/mps/SponListItem.js
+++ src/components/mps/SponListItem.js
@@ -17,8 +17,8 @@ export default function SponListItem(props) {
           <div class="vote-list-header">
             <div class="vote-list-title">{`Vote No. ${props.vote_num}`}</div>
           
-            {props.result==="Negatived" && <div class="vote-disagree">Resulted in disagreed to <FontAwesomeIcon icon={faCheck} /></div>}
-            {props.result==="Agreed To" && <div class="vote-agree">Resulted in agreed to <FontAwesomeIcon icon={faTimes} /></div>}
+            {props.result==="Negatived" && <div class="vote-disagree">Result: Negatived <FontAwesomeIcon icon={faCheck} /></div>}
+            {props.result==="Agreed To" && <div class="vote-agree">Result: Agreed To <FontAwesomeIcon icon={faTimes} /></div>}
           </div>
         
           <div class="vote-list-summary">{props.summary}</div>
